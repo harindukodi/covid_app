540a92f1b12e88d8022e2fba4129a70d3bd74d3e
Harindu <harindukodi@outlook.com>
2020-05-02 00:31:06 +1000

    Initializing the app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Reaact from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>App</h1>
+            </div>
+        )
+    }
+}
